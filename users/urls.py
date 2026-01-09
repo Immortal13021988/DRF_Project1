b4310f5bc42950a8c1c6d9b9f519f1e233f14818
iua.suranov <pradedura@mail.ru>
2026-01-10 02:57:53 +0300

    add users

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from .apps import UsersConfig
+from django.urls import path
+
+app_name = UsersConfig.name
+
+#
+# urlpatterns = [
+#     path("register/", RegisterView.as_view(), name="register"),
+#     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
+#     path("logout/", LogoutView.as_view(next_page="catalog:prod_list"), name="logout"),
+# ]
