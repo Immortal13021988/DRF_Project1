ea14e2a541a0355ac0f046561686e59e390fcbc9
iua.suranov <pradedura@mail.ru>
2026-01-10 17:58:05 +0300

    fix urls

--- lms/urls.py
+++ lms/urls.py
@@ -1,4 +1,26 @@
-from .apps import LmsConfig
 from django.urls import path
-#
-# app_name = LmsConfig.name
+from rest_framework.routers import SimpleRouter
+
+from .apps import LmsConfig
+from .views import (CourseViewSet, LessonCreateApiView, LessonDestroyApiView,
+                    LessonListApiView, LessonRetrieveApiView,
+                    LessonUpdateApiView)
+
+app_name = LmsConfig.name
+router = SimpleRouter()
+
+router.register("", CourseViewSet)
+
+urlpatterns = [
+    path("lesson/", LessonListApiView.as_view(), name="lesson_list"),
+    path("lesson/<int:pk>/", LessonRetrieveApiView.as_view(), name="lesson_retrieve"),
+    path("lesson/create/", LessonCreateApiView.as_view(), name="lesson_create"),
+    path(
+        "lesson/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lesson_update"
+    ),
+    path(
+        "lesson/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lesson_delete"
+    ),
+]
+
+urlpatterns += router.urls
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,12 @@
-from .apps import UsersConfig
 from django.urls import path
+
+from .apps import UsersConfig
 
 app_name = UsersConfig.name
 
 #
-# urlpatterns = [
-#     path("register/", RegisterView.as_view(), name="register"),
-#     path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
-#     path("logout/", LogoutView.as_view(next_page="catalog:prod_list"), name="logout"),
-# ]
+urlpatterns = [
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="catalog:prod_list"), name="logout"),
+]
