8aa4e2a83206cd5e85268277779916b31b894caa
iua.suranov <pradedura@mail.ru>
2026-01-17 02:38:24 +0300

    add lessons on courses and count lessons

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,9 +1,11 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from .models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    """Сериализатор по курсам"""
+
     class Meta:
         model = Course
         fields = "__all__"
@@ -13,3 +15,16 @@ class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseDetailSerializer(ModelSerializer):
+    """Сериализатор по курсам с добавлением поля с количеством уроков"""
+    lessons = LessonSerializer(many=True, read_only=True)
+    lesson_count = SerializerMethodField()
+
+    def get_lesson_count(self, course):
+        return course.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ["title", "description", "preview", "lessons", "lesson_count"]
