71137ab77acfd6a45b4a0e4859caf0f3e3a5a999
iua.suranov <pradedura@mail.ru>
2026-01-21 01:42:43 +0300

    fix linters

--- users/urls.py
+++ users/urls.py
@@ -18,7 +18,9 @@ urlpatterns = [
     # path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     # path("logout/", LogoutView.as_view(next_page="catalog:prod_list"), name="logout"),
     path(
-        "login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
     ),
     path(
         "token/refresh/",
