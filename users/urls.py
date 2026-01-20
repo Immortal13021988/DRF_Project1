f98de8bb652737cd0e945ee704bc23416471c0a5
iua.suranov <pradedura@mail.ru>
2026-01-20 23:49:42 +0300

    fix urls

--- users/urls.py
+++ users/urls.py
@@ -18,11 +18,11 @@ urlpatterns = [
     # path("login/", LoginView.as_view(template_name="users/login.html"), name="login"),
     # path("logout/", LogoutView.as_view(next_page="catalog:prod_list"), name="logout"),
     path(
-        "login/", TokenObtainPairView.as_view(permission_classes=AllowAny), name="login"
+        "login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"
     ),
     path(
         "token/refresh/",
-        TokenRefreshView.as_view(permission_classes=AllowAny),
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
         name="token_refresh",
     ),
 ]
