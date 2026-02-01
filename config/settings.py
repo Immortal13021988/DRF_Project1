a59a48cfce9d8918054fec72b3feeaa07756432a
iua.suranov <pradedura@mail.ru>
2026-02-01 23:59:14 +0300

    add redis and celery

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "django_filters",
     "rest_framework_simplejwt",
+    "django_celery_beat",
+
     "users",
     "lms",
 ]
@@ -123,7 +125,7 @@ LOGIN_REDIRECT_URL = "#"
 LOGOUT_REDIRECT_URL = "#"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 
@@ -136,3 +138,30 @@ SPECTACULAR_SETTINGS = {
 }
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+# Настройки для Celery
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://localhost:6379'  # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = TIME_ZONE
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
+
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+# Настройки для Celery-beat
+# CELERY_BEAT_SCHEDULE = {
+#     "task-name": {
+#         "task": "lms.tasks.my_task",  # Путь к задаче
+#         "schedule": timedelta(minutes=10),  # Расписание выполнения задачи (например, каждые 10 минут)
+#     },
+# }
